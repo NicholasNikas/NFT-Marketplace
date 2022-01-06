7582202dbf2c9d6d6ca7cf7c513f38fb2a303b5f
Nicholas Nikas <nicholasnikas@Nicholass-MacBook-Pro.local>
2022-01-06 01:23:00 -0500

    Add minting functionality

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC721 {
-
     /*
     building out the minting function:
     a. nft to point to an address
@@ -11,6 +9,42 @@ contract ERC721 {
     d. keep track of how many tokens an owner address has
     e. create an event that emits a transfer log - contract address, where 
        its being minted to, the id
-
     */
+
+contract ERC721 {
+
+    event Transfer(
+        address indexed from, 
+        address indexed to, 
+        uint256 indexed tokenId
+    );
+
+    // mapping in solidity creates a hash table of key pair values
+
+    // Mapping from token id to owner
+    mapping(uint256 => address) private _tokenOwner;
+
+    // Mapping from owner to number of owned tokens
+    mapping(address => uint256) private _ownedTokensCount;
+
+    function _exists(uint256 tokenId) internal view returns(bool) {
+        // setting address of NFT owner to check the mapping 
+        // of the address from tokenOwner to tokenId
+        address owner = _tokenOwner[tokenId];
+        // return truthiness that address is not zero
+        return owner != address(0);
+    }
+
+    function _mint(address to, uint256 tokenId) internal {
+        // requires that the address isn't zero
+        require(to != address(0),'ERC721: minting to the zero address');
+        // requires that the token does not already exist
+        require(!_exists(tokenId), 'ERC721: token already minted');
+        // we are adding a new address with a token id for minting
+        _tokenOwner[tokenId] = to;
+        // keeping track of each address thsat is minting and adding one to the count
+        _ownedTokensCount[to] += 1;
+
+        emit Transfer(address(0), to, tokenId);
+    }
 }
--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -4,9 +4,25 @@ pragma solidity ^0.8.0;
 import './ERC721Connector.sol';
 
 contract KryptoBird is ERC721Connector {
+    
+    // array to store our nfts
+    string[] public kryptoBirdz;
 
-    // initialize contract to inherit name and symbol from erc721 data so that
-    // the name is KryptoBird and the symbol is KBIRDZ
+    mapping(string => bool) _kryptoBirdzExists;
+
+    function mint(string memory _kryptoBird) public {
+        require(!_kryptoBirdzExists[_kryptoBird],'Error: KryptoBird already exists');
+
+        // this is deprecated - uint _id = kryptoBirdz.push(_kryptoBird);
+        // .push no longer returns the length but a ref to the added element
+
+        kryptoBirdz.push(_kryptoBird);
+        uint _id = kryptoBirdz.length -1;
+
+        _mint(msg.sender, _id);
+
+        _kryptoBirdzExists[_kryptoBird] = true;
+    }
 
     constructor() ERC721Connector('KryptoBird', 'KBIRDZ') {}
 }
